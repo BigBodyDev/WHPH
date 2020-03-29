2dc345fc3b372ac0fbf68ceec80b0a198a4cb0d8
Devin Green <dev.green2@gmail.com>
2020-03-28 17:43:24 -0700

    Initial Commit

---
+++ WHPH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WHPH
+//
+//  Created by Devin Green on 3/28/20.
+//  Copyright © 2020 Devin Green. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
